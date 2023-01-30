b6d860f03e6acfe685e00edc57b78d062f4e6f2a
“tsinghua-lau <408285649@qq.com>
2023-01-30 16:58:15 +0800

    💤feat:新增路由图标

--- src/plugins/icon.ts
+++ src/plugins/icon.ts
@@ -3,7 +3,7 @@ import { createFromIconfontCN } from '@ant-design/icons-vue';
 
 const Icon = createFromIconfontCN({
     // 阿里图标库
-    scriptUrl: '//at.alicdn.com/t/c/font_3869461_znnbv0vu4j.js',
+    scriptUrl: '//at.alicdn.com/t/c/font_3869461_55nlvb9o8da.js',
 });
 
 export default {
