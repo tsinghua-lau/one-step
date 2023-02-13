d3a7af4083c65e97ca752107faf8376ed0fd98ac
“tsinghua-lau <408285649@qq.com>
2023-02-13 10:37:54 +0800

    🐛fix:antd 卡顿白屏bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,78 +1,106 @@
-import { defineConfig } from "vite";
-import AutoImport from "unplugin-auto-import/vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
-import eslintPlugin from "vite-plugin-eslint";
-import { visualizer } from "rollup-plugin-visualizer";
-import Components from "unplugin-vue-components/vite";
-import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from 'vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import eslintPlugin from 'vite-plugin-eslint';
+import { visualizer } from 'rollup-plugin-visualizer';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 /** 路径查找 */
 const pathResolve = (dir: string): string => {
-  return resolve(__dirname, ".", dir);
+    return resolve(__dirname, '.', dir);
 };
 
 /** 设置别名 */
 const alias: Record<string, string> = {
-  "@": pathResolve("src"),
+    '@': pathResolve('src'),
 };
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    visualizer({
-      emitFile: true, //是否被触摸
-      filename: "test.html", //生成分析网页文件名
-      open: true, //在默认用户代理中打开生成的文件
-      gzipSize: true, //从源代码中收集 gzip 大小并将其显示在图表中
-      brotliSize: true, //从源代码中收集 brotli 大小并将其显示在图表中
-    }),
+    plugins: [
+        vue(),
+        visualizer({
+            emitFile: true, //是否被触摸
+            filename: 'test.html', //生成分析网页文件名
+            open: true, //在默认用户代理中打开生成的文件
+            gzipSize: true, //从源代码中收集 gzip 大小并将其显示在图表中
+            brotliSize: true, //从源代码中收集 brotli 大小并将其显示在图表中
+        }),
 
-    createSvgIconsPlugin({
-      // 指定存放svg的文件夹路径
-      iconDirs: [resolve(process.cwd(), "src/assets/svg/icons")],
-      // 指定symbolId格式
-      symbolId: "icon-[name]",
-    }),
-    AutoImport({
-      imports: ["vue"],
-      dts: "src/auto-import.d.ts",
-    }),
-    Components({
-      resolvers: [AntDesignVueResolver()],
-    }),
-    eslintPlugin({
-      include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"],
-      cache: false,
-    }),
-  ],
-  build: {
-    emptyOutDir: true,
-    chunkSizeWarningLimit: 1500,
-    rollupOptions: {
-      output: {
-        manualChunks(id: string) {
-          if (id.includes("node_modules")) {
-            return id.split("/node_modules/").pop()?.split("/")[0];
-          }
+        createSvgIconsPlugin({
+            // 指定存放svg的文件夹路径
+            iconDirs: [resolve(process.cwd(), 'src/assets/svg/icons')],
+            // 指定symbolId格式
+            symbolId: 'icon-[name]',
+        }),
+
+        AutoImport({
+            resolvers: [AntDesignVueResolver()],
+            imports: ['vue', 'vue-router', 'vue/macros', '@vueuse/core'],
+            dts: 'src/auto-import.d.ts',
+        }),
+
+        Components({
+            // 生产环境按需导入
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            resolvers: process.env.NODE_ENV === 'production' ? AntDesignVueResolver() : undefined,
+            // allow auto load markdown components under `./src/components/`
+            extensions: ['vue'],
+            // allow auto import and register components used in markdown
+            include: [/\.vue$/, /\.vue\?vue/],
+            dts: 'src/components.d.ts',
+        }),
+
+        // 开发环境完整导入ant-design-vue
+        {
+            name: 'dev-auto-import-ant-design-vue',
+            transform(code, id) {
+                if (process.env.NODE_ENV !== 'production' && /src\/main.ts$/.test(id)) {
+                    return {
+                        code: `${code};import Antd from 'ant-design-vue';import 'ant-design-vue/dist/antd.css';app.use(Antd);`,
+                        map: null,
+                    };
+                }
+            },
         },
-      },
-    },
-  },
-  server: {
-    // 配置代理服务器，解决跨域
-    port: 8088,
-    proxy: {
-      "/api": {
-        target: "http://139.198.176.206:1980",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 
-  resolve: {
-    alias,
-  },
+        eslintPlugin({
+            include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
+            cache: false,
+        }),
+    ],
+    build: {
+        emptyOutDir: true,
+        chunkSizeWarningLimit: 1500,
+        rollupOptions: {
+            output: {
+                manualChunks(id: string) {
+                    if (id.includes('ant-design-vue/es')) {
+                        return 'ant-design-vue';
+                    }
+                    if (id.includes('node_modules')) {
+                        return id.split('/node_modules/').pop()?.split('/')[0];
+                    }
+                },
+            },
+        },
+    },
+    server: {
+        // 配置代理服务器，解决跨域
+        port: 8088,
+        proxy: {
+            '/api': {
+                target: 'http://139.198.176.206:1980',
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+
+    resolve: {
+        alias,
+    },
 });
